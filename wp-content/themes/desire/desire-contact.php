1253aa8ed6128a6784df421c82b8f870e41a31b5
formella.alex <formella.alex@gmail.com>
2017-06-25 18:02:01 +0200

    Before technicall changes in php

--- wp-content/themes/desire/desire-contact.php
+++ wp-content/themes/desire/desire-contact.php
@@ -6,34 +6,42 @@
 <div id="contact-container">
     <div class="contact-content">
         <div class="row">
-            <div class="col-md-5 offset-md-3">
+            <div class="col-md-8 offset-md-2">
                 <div class="col-md-12">
                     <div class="contact-form">
-                        <p class="contact-title text-center"> LET'S START THE CONVERSATION </p>
-                        <form class="form-group">
-                            <input type="text" class="form-control" placeholder="NAME"/>
-                            <input type="text" class="form-control" placeholder="EMAIL"/>
-                            <input type="text" class="form-control" placeholder="PHONE"/>
-                            <input type="text" class="form-control" placeholder="TOPIC"/>
-                            <div class="contact-message-container">
-                                <h6 class="contact-message-label">MESSAGE</h6>
-                                <textarea class="form-control" placeholder="Message"> </textarea>
-                                <button class="pull-right"> <h5> SUBMIT </h5></button>
+                        <div class="col-md-9 no-margin">
+                            <p class="contact-title text-center"> LET'S START THE CONVERSATION </p>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-9">
+                                <form class="form-group">
+                                    <input type="text" class="form-control" placeholder="NAME"/>
+                                    <input type="text" class="form-control" placeholder="EMAIL"/>
+                                    <input type="text" class="form-control" placeholder="PHONE"/>
+                                    <input type="text" class="form-control" placeholder="TOPIC"/>
+                                    <div class="contact-message-container">
+                                        <h6 class="contact-message-label">MESSAGE</h6>
+                                        <textarea class="form-control" placeholder="Message"> </textarea>
+                                        <button class="pull-right"> <h5> SUBMIT </h5></button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                            <div class="col-md-3">
+                                <div class="contact-text">
+                                    <h6 class="title">EMAIL</h6>
+                                    <h6 class="subtitle">info@desireglass.com</h6> <br />
+                                    <h6 class="title">PHONE</h6>
+                                    <h6 class="subtitle">+48 505 608 996</h6>
+                                    <h6 class="subtitle">+48 506 336 182</h6> <br />
+                                    <h6 class="title">ADDRES</h6>
+                                    <h6 class="subtitle">Al. Grunwaldzka 481</h6>
+                                    <h6 class="subtitle">Gdansk 80-229</h6>
+                                    <h6 class="subtitle">POLAND</h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <h6>EMAIL</h6>
-                <h6>info@desireglass.com</h6> <br />
-                <h6>PHONE</h6>
-                <h6>+48 505 608 996</h6>
-                <h6>+48 506 336 182</h6> <br />
-                <h6>ADDRES</h6>
-                <h6>Al. Grunwaldzka 481</h6>
-                <h6>Gdansk 80-229</h6>
-                <h6>POLAND</h6>
             </div>
         </div>
     </div>
--- wp-content/themes/desire/o-nas-template.php
+++ wp-content/themes/desire/o-nas-template.php
@@ -4,8 +4,8 @@
  */
 ?>
 
-<div class="o-nas-content-container">
-    <div class="o-nas-content col-md-8 offset-md-2 pull-right">
+<div class="about-content-container">
+    <div class="about-content col-md-8 offset-md-2 pull-right">
         <?php while (have_posts()) : the_post(); ?>
             <?php get_template_part('templates/content', 'page'); ?>
         <?php endwhile; ?>
